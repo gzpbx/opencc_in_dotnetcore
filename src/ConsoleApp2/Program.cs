3c42029eb9796d043a64c5119632f4349cfc7794
Boler <gzpbx69@gmail.com>
2016-07-31 22:00:25 +0800

    all solution

---
+++ src/ConsoleApp2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+
+            IntPtr o = opencc_open(@"E:\opencc\s2t.json");
+
+            if (o != IntPtr.Zero && o != new IntPtr(-1))
+            {
+                var str = "龙，头发 发现";
+                Console.WriteLine(str);
+                var input = Encoding.UTF8.GetBytes(str);
+                IntPtr output = opencc_convert_utf8(o, input, -1);
+                var result = PtrToStringUtf8(output);
+                opencc_convert_utf8_free(output);
+                Console.WriteLine(result);
+            }
+
+
+            Console.WriteLine("Press any key to exit !!");
+            Console.ReadKey();
+        }
+
+        private static string PtrToStringUtf8(IntPtr ptr) // aPtr is nul-terminated
+        {
+            if (ptr == IntPtr.Zero)
+                return "";
+            int len = 0;
+            while (Marshal.ReadByte(ptr, len) != 0)
+                len++;
+            if (len == 0)
+                return "";
+            byte[] array = new byte[len];
+            Marshal.Copy(ptr, array, 0, len);
+            return System.Text.Encoding.UTF8.GetString(array);
+        }
+
+        const string DLL_NAME_64 = "opencc.dll";
+
+        [DllImport(DLL_NAME_64, EntryPoint = "opencc_open", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr opencc_open(string configFileName);
+
+        [DllImport(DLL_NAME_64, EntryPoint = "opencc_convert_utf8", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr opencc_convert_utf8(IntPtr opencc, [MarshalAs(UnmanagedType.LPArray)]byte[] input, long length);
+
+        [DllImport(DLL_NAME_64, EntryPoint = "opencc_convert_utf8_free", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void opencc_convert_utf8_free(IntPtr str);
+
+    }
+}
