ddbdac0e713fb3ba1733cae368a3567682257fc2
Boler <gzpbx69@gmail.com>
2016-07-31 22:36:16 +0800

    Support Linux

--- src/ConsoleApp2/Program.cs
+++ src/ConsoleApp2/Program.cs
@@ -14,32 +14,60 @@ namespace ConsoleApp2
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-            if (File.Exists(DLL_NAME_64))
-            {
-                IntPtr o = opencc_open(@"../../opencc/s2t.json");
+            var str = "龙，头发 发现";
+            Console.WriteLine(str);
+            Console.WriteLine(BasicToTraditional(str));
 
-                if (o != IntPtr.Zero && o != new IntPtr(-1))
+            Console.WriteLine("Press any key to exit !!");
+            Console.ReadKey();
+        }
+
+        private static string BasicToTraditional(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                IntPtr o_win = opencc_open_win(@"../../opencc/s2t.json");
+
+                if (o_win != IntPtr.Zero && o_win != new IntPtr(-1))
                 {
-                    var str = "龙，头发 发现";
-                    Console.WriteLine(str);
-                    var input = Encoding.UTF8.GetBytes(str);
-                    IntPtr output = opencc_convert_utf8(o, input, -1);
+                    IntPtr output = opencc_convert_utf8_win(o_win, Encoding.UTF8.GetBytes(input), -1);
                     var result = PtrToStringUtf8(output);
-                    opencc_convert_utf8_free(output);
-                    Console.WriteLine(result);
+                    opencc_convert_utf8_free_win(output);
+                    return result;                }
+                else
+                {
+                    Console.WriteLine("something is wrong !! ");
+                }
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                // make sure execute the command below first -
+                // sudo ln -s /usr/lib/libopencc.so.2 /usr/lib64/libopencc.so.2
+                IntPtr o_win = opencc_open_linux("s2t.json");
+
+                if (o_win != IntPtr.Zero && o_win != new IntPtr(-1))
+                {
+                    IntPtr output = opencc_convert_utf8_linux(o_win, Encoding.UTF8.GetBytes(input), -1);
+                    var result = PtrToStringUtf8(output);
+                    opencc_convert_utf8_free_linux(output);
+                    return result;
                 }
                 else
                 {
                     Console.WriteLine("something is wrong !! ");
                 }
             }
-            else
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                Console.WriteLine("opencc.dll doesn't exits !!, CurrentDirectory: " + Directory.GetCurrentDirectory());
+                return input;
             }
 
-            Console.WriteLine("Press any key to exit !!");
-            Console.ReadKey();
+            return input;
         }
 
         private static string PtrToStringUtf8(IntPtr ptr) // aPtr is nul-terminated
@@ -56,16 +84,28 @@ namespace ConsoleApp2
             return System.Text.Encoding.UTF8.GetString(array);
         }
 
-        const string DLL_NAME_64 = "../../opencc/opencc.dll";
+        const string DLL_NAME_64_Win = "../../opencc/opencc.dll";
 
-        [DllImport(DLL_NAME_64, EntryPoint = "opencc_open", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr opencc_open(string configFileName);
+        [DllImport(DLL_NAME_64_Win, EntryPoint = "opencc_open", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr opencc_open_win(string configFileName);
 
-        [DllImport(DLL_NAME_64, EntryPoint = "opencc_convert_utf8", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr opencc_convert_utf8(IntPtr opencc, [MarshalAs(UnmanagedType.LPArray)]byte[] input, long length);
+        [DllImport(DLL_NAME_64_Win, EntryPoint = "opencc_convert_utf8", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr opencc_convert_utf8_win(IntPtr opencc, [MarshalAs(UnmanagedType.LPArray)]byte[] input, long length);
 
-        [DllImport(DLL_NAME_64, EntryPoint = "opencc_convert_utf8_free", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void opencc_convert_utf8_free(IntPtr str);
+        [DllImport(DLL_NAME_64_Win, EntryPoint = "opencc_convert_utf8_free", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void opencc_convert_utf8_free_win(IntPtr str);
+
+
+        const string DLL_NAME_64_Linux = "libopencc.so.2";
+
+        [DllImport(DLL_NAME_64_Linux, EntryPoint = "opencc_open", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr opencc_open_linux(string configFileName);
+
+        [DllImport(DLL_NAME_64_Linux, EntryPoint = "opencc_convert_utf8", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr opencc_convert_utf8_linux(IntPtr opencc, [MarshalAs(UnmanagedType.LPArray)]byte[] input, long length);
+
+        [DllImport(DLL_NAME_64_Linux, EntryPoint = "opencc_convert_utf8_free", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void opencc_convert_utf8_free_linux(IntPtr str);
 
     }
 }
